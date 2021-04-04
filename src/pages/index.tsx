09a9b43af01f448915aba1d2d2da43a8760ef27d
Jack LaFond <jack.lafond@icloud.com>
2021-04-04 10:39:38 -0400

    very very very early UI rewrite

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,55 @@
 import MainLayout from "../layouts/main";
 import Link from "next/link";
+import { GitHub, Twitter } from "react-feather";
 
 export default function Home() {
   return (
     <MainLayout>
-      <h1 className="text-xl font-bold">tools</h1>
-      <a href="https://github.com/alii/tools" className="text-blue-500 underline">
-        view on github
-      </a>
-      <ul className="list-disc">
-        <li>
-          <Link href="/lookup">discord lookup</Link>
-        </li>
-        <li>
-          <Link href="/json-ts">json to typescript</Link>
-        </li>
-      </ul>
+      <h1 className="text-left pt-8 font-bold text-5xl pb-4">
+        uwu.<span className="text-red-500">red</span> toolkit
+      </h1>
+      <div className="flex">
+        <div className="flex space-x-2">
+          <GitHub className="text-black-500 hover:text-red-500 transition ease-in-out" />
+          <span>
+            <Twitter />
+            pog
+          </span>
+        </div>
+      </div>
+
+      <div className="flex mt-4">
+        <ul className="list-disc">
+          <Link href="/lookup">
+            <div className="w-auto border hover:border-red-100 transition ease-in-out bg-gray-100 hover:bg-red-50 hover:text-red-500 rounded-md placeholder-red-300 p-3">
+              <div>
+                <div className="">
+                  <h1 className="font-semibold text-sm sm:text-regular">
+                    Discord ID Lookup
+                  </h1>
+                  <p className="text-xs">Look up Discord ID.</p>
+                </div>
+              </div>
+            </div>
+          </Link>
+          <Link href="/json-ts">
+            <div className="flex mt-4">
+              <div className="w-auto border hover:border-red-100 transition ease-in-out bg-gray-100 hover:bg-red-50 hover:text-red-500 rounded-md placeholder-red-300 p-3">
+                <div>
+                  <div className="">
+                    <h1 className="font-semibold text-sm sm:text-regular">
+                      JSON to TypeScript object
+                    </h1>
+                    <p className="text-xs">
+                      Convert JSON to a TypeScript type object.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </Link>
+        </ul>
+      </div>
     </MainLayout>
   );
 }
